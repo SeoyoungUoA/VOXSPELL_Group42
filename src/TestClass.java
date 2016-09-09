da253400dcf955ff15dfb78f8339ef6b2d1987cb
Dylan <dylan.hall077@gmail.com>
2016-09-09 15:05:19 +1200

    Commit a new method testMethod

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,5 +4,11 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         //Dylan Commit;
+        testMethod();
+    }
+
+    //Worked on by dylan - pls review
+    public static void testMethod(){
+        System.out.print("Testing");
     }
 }
