be4489d14d027774bd34d983c579adb51c4f2b1e
Dylan <dhal525@aucklanduni.ac.nz>
2016-09-12 20:55:08 +1200

    Adding new branch with a class, then going to merge branches

---
+++ src/WordTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by dylan on 12/09/16.
+ * This is to test MERGING this branch into a older one.
+ */
+public class WordTest {
+    //Hold other stuff in here
+    //festival etc etc....
+    public void speak(){
+        //festival commands
+    }
+}
