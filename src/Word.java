2c680a11e8ab22e521d67ec3a6acfa4a9492503a
Dylan <dhal525@aucklanduni.ac.nz>
2016-09-12 20:41:45 +1200

    Committed a new method, chwecking branch working again

--- src/Word.java
+++ src/Word.java
@@ -5,9 +5,14 @@ public class Word {
     //Testing for making a new class on a new branch
     private String _name;
     private int _level;
+    private int _mastered;
 
     Word(String name, int level){
         _name = name; //rip
         _level = level;
+    }
+
+    public void mastered(){
+        _mastered++;
     }
 }
